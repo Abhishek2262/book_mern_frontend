e5a30cf018839c2f4d729a05857b74c307d85d93
Abhishek Pandey <abhishekpandey1480@gmail.com>
2023-12-24 15:58:09 +0530

    Project new

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content:[
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+     './pages/**/*.{html,js,jsx,ts,tsx}',
+      './components/**/*.{html,js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
